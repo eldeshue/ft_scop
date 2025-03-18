bd0c829c91f3a3f94d627e9a8a554f15e626c421
iphuck22 <eldeshue@naver.com>
2025-03-18 18:45:51 +0900

    feat: initialize GLFW, create window object

--- src/main.cc
+++ src/main.cc
@@ -8,5 +8,30 @@ extern "C" {
 
 int main()
 {
-	std::cout << "Hello World\n";
+	/*------------------------- GLFW ---------------------------------*/
+	// glfw init
+	glfwInit();
+
+	// glfWindowHint
+	// first arg : type of option
+	// secondactual value of option
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	// major version of OpenGL, 3,0
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	// minor version of OpenGL, 0.3
+
+	// OpenGL mode, core-profile mode
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// for mac os
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// create window object
+	GLFWwindow* hWindow = glfwCreateWindow(800, 600, "ft_scop", NULL, NULL);
+	if (hWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(hWindow);
+
 }
