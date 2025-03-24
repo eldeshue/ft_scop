1923e61391962f14d0f356df455ea3c890872f6d
iphuck22 <eldeshue@naver.com>
2025-03-24 19:48:33 +0900

    feat : add fragment shader, refactoring

--- src/main.cc
+++ src/main.cc
@@ -23,6 +23,10 @@ constexpr const int VIEWPORT_HEIGHT = 600;
 void SetGLFWOption();
 void RegisterInputEvent(GLFWwindow* const hWindow);
 void HandleInput(GLFWwindow* const hWindow);
+
+GLuint CreateVertexShader(char const* const shaderSource);
+GLuint CreateFragmentShader(char const* const shaderSource);
+
 void RenderObject();
 
 int main(int argc, char* argv[])
@@ -95,27 +99,27 @@ int main(int argc, char* argv[])
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);	// no change, multiple rendering
 
 	// shader compile and loading
-	char const* const shaderSource =
+	char const* const vertexShaderSource =
 		"#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\0";
+	char const* const fragmentShaderSource =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{ FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";	// decide color of fragment orange
 
-	GLuint hVertexShader = glCreateShader(GL_VERTEX_SHADER);// create vertex shader handle
-	glShaderSource(hVertexShader, 1, &shaderSource, NULL);	// set multiple shader source file
-	glCompileShader(hVertexShader);							// compile and link
-
-	int isVertexShaderCompileSuccess = 0;
-	char infoLog[512];
-	glGetShaderiv(hVertexShader, GL_COMPILE_STATUS, &isVertexShaderCompileSuccess);
-	if (!isVertexShaderCompileSuccess)
+	GLuint hVShader = CreateVertexShader(vertexShaderSource);
+	GLuint hFShader = CreateFragmentShader(fragmentShaderSource);
+	if (hVShader == -1 || hFShader == -1)
 	{
-		glGetShaderInfoLog(hVertexShader, 512, NULL, infoLog);
-		std::cerr << "Error : vertex shader compilation failed\n" << infoLog << std::endl;
-		return -1;
+		// shader compile failed
+		exit(1);
 	}
+
 
 	// render loop, each iteration consist a frame
 	while (!glfwWindowShouldClose(hWindow))
@@ -182,6 +186,48 @@ void HandleInput(GLFWwindow* const hWindow)
 	// esc key input
 	if (glfwGetKey(hWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(hWindow, true);
+}
+
+/**
+ * @brief get GLSL source string and create vertex shader
+ */
+GLuint CreateVertexShader(char const* const shaderSource)
+{
+	GLuint hVertexShader = glCreateShader(GL_VERTEX_SHADER);// create vertex shader handle
+	glShaderSource(hVertexShader, 1, &shaderSource, NULL);	// set multiple shader source file
+	glCompileShader(hVertexShader);							// compile and link
+
+	int isVertexShaderCompileSuccess = 0;
+	char infoLog[512];
+	glGetShaderiv(hVertexShader, GL_COMPILE_STATUS, &isVertexShaderCompileSuccess);
+	if (!isVertexShaderCompileSuccess)
+	{
+		glGetShaderInfoLog(hVertexShader, 512, NULL, infoLog);
+		std::cerr << "Error : vertex shader compilation failed\n" << infoLog << std::endl;
+		return -1;
+	}
+	return hVertexShader;
+}
+
+/**
+ * @brief get GLSL source string and create fragment shader
+ */
+GLuint CreateFragmentShader(char const* const shaderSource)
+{
+	GLuint hFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(hFragmentShader, 1, &shaderSource, NULL);
+	glCompileShader(hFragmentShader);
+
+	int isFragmentShaderCompileSuccess = 0;
+	char infoLog[512];
+	glGetShaderiv(hFragmentShader, GL_COMPILE_STATUS, &isFragmentShaderCompileSuccess);
+	if (!isFragmentShaderCompileSuccess)
+	{
+		glGetShaderInfoLog(hFragmentShader, 512, NULL, infoLog);
+		std::cerr << "Error : fragment shader compilation failed\n" << infoLog << std::endl;
+		return -1;
+	}
+	return hFragmentShader;
 }
 
 /**
