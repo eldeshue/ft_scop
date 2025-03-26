1d2fd2944b3f5d1048c665f6fe24c5beba8d62f2
iphuck22 <eldeshue@naver.com>
2025-03-26 18:13:21 +0900

    feat : rendering triangle done
    렌더링 자원 생성 과정 함수로 추상화
    삼각형 4개 rendering하도록 데이터 추가

--- src/main.cc
+++ src/main.cc
@@ -24,16 +24,22 @@ void SetGLFWOption();
 void RegisterInputEvent(GLFWwindow* const hWindow);
 void HandleInput(GLFWwindow* const hWindow);
 
-GLuint CreateVertexShader(char const* const shaderSource);
-GLuint CreateFragmentShader(char const* const shaderSource);
-
-void RenderObject();
-
 typedef struct Point {
 	float x;
 	float y;
 	float z;
 } Point;
+
+GLuint CreateBO();
+GLuint CreateVBO(std::vector<Point> const& vertices);
+GLuint CreateEBO(std::vector<uint32_t> const& indices);
+GLuint CreateVAO(GLuint const hVBO, GLuint const hEBO);
+
+GLuint CreateVertexShader(char const* const shaderSource);
+GLuint CreateFragmentShader(char const* const shaderSource);
+GLuint CreateShaderProgram(GLuint const hVShader, GLuint const hFShader);
+
+void RenderTriangles(GLsizei const numTri, GLuint const hVAO, GLuint const hShaderProgram);
 
 int main(int argc, char* argv[])
 {
@@ -63,19 +69,22 @@ int main(int argc, char* argv[])
 
 	// Get render resource
 	// vertices, render topology, texture(?)
-	std::array<Point, 4> vertices = { {
+	std::vector<Point> vertices = {
 		{-0.5f, -0.5f, 0.0f},
 		{0.5f, -0.5f, 0.0f},
 		{0.0f, 0.5f, 0.0f},
-		{0.0f, -0.8f, 0.0f}
-	} };
-
-	std::array<uint32_t, 6> indices = {
-		0, 1, 2,
-		0, 1, 3
+		{0.3f, -0.8f, 0.0f},
+		{1.0f, 0.1f, 0.0f}
 	};
 
-	std::cout << "debug : " << sizeof(std::array<Point, 3>) << '\n';
+	std::vector<uint32_t> indices1 = {
+		0, 2, 1,
+		0, 1, 3,
+	};
+	std::vector<uint32_t> indices2 = {
+		2, 1, 4,
+		1, 3, 4
+	};
 
 	/*-------------------------- GLFW --------------------------------*/
 	// set glfw option
@@ -113,54 +122,40 @@ int main(int argc, char* argv[])
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\0";
-	char const* const fragmentShaderSource =
+	char const* const fragmentShaderSource1 =
 		"#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"void main()\n"
 		"{ FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";	// decide color of fragment, orange
+	char const* const fragmentShaderSource2 =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{ FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);}\0";	// decide color of fragment, orange
 
 	GLuint hVShader = CreateVertexShader(vertexShaderSource);
-	GLuint hFShader = CreateFragmentShader(fragmentShaderSource);
-	if (hVShader == 0 || hFShader == 0)
+	GLuint hFShader1 = CreateFragmentShader(fragmentShaderSource1);
+	GLuint hFShader2 = CreateFragmentShader(fragmentShaderSource2);
+	if (hVShader == 0 || hFShader1 == 0 || hFShader2 == 0)
 	{
 		// shader compile failed
 		exit(1);
 	}
 
-	// assemble pipeline
-	GLuint hShaderProgram = glCreateProgram();
-	glAttachShader(hShaderProgram, hVShader);
-	glAttachShader(hShaderProgram, hFShader);
-	glLinkProgram(hShaderProgram);	// link shaders, create executable
-	int isShaderLinkSuccess = 0;
-	glGetProgramiv(hShaderProgram, GL_LINK_STATUS, &isShaderLinkSuccess);
-	if (!isShaderLinkSuccess) {
-		//		glGetProgramInfoLog(hShaderProgram, 512, NULL, infoLog);
-		std::cerr << "Error : shader link failed." << std::endl;
-		return 1;
-	}
+	GLuint hShaderProgram1 = CreateShaderProgram(hVShader, hFShader1);
+	GLuint hShaderProgram2 = CreateShaderProgram(hVShader, hFShader2);
+	if (hShaderProgram1 == 0 || hShaderProgram2 == 0) return 1;
+
 	glDeleteShader(hVShader);
-	glDeleteShader(hFShader);	// after link, no need shader object(reuse otherwise)
+	glDeleteShader(hFShader1);	// after link, no need shader object(reuse otherwise)
+	glDeleteShader(hFShader2);
 
-	// create VAO
-	GLuint hVAO, hVBO, hEBO;
-	glGenVertexArrays(1, &hVAO);
-	glGenBuffers(1, &hVBO);	// create buffer for VBO
-	glGenBuffers(1, &hEBO);	// create buffer for EBO
-
-	glBindVertexArray(hVAO);	// begin, bind VAO
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, hEBO);	// element(index) buffer
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, hVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);	// no change, multiple rendering
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Point), reinterpret_cast<void*>(0));	// map vbo and shader program input at 0
-	glEnableVertexAttribArray(0); // enable layout of location 0, if another location exist another call needed
-
-	glBindVertexArray(0);	// end, unbind VAO
-
-	std::cout << "debug : " << sizeof(Point) << ' ' << 3 * sizeof(float) << '\n';
+	// init render resources
+	GLuint const hVBO = CreateVBO(vertices);
+	GLuint const hEBO1 = CreateEBO(indices1);
+	GLuint const hEBO2 = CreateEBO(indices2);
+	GLuint const hVAO1 = CreateVAO(hVBO, hEBO1);
+	GLuint const hVAO2 = CreateVAO(hVBO, hEBO2);
 
 	/* ------------------------------- Define Scene -----------------------------------*/
 
@@ -171,25 +166,25 @@ int main(int argc, char* argv[])
 	{
 		HandleInput(hWindow);
 
-		// render routine start
-		RenderObject();
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(hShaderProgram);	// bind shader program
-		glBindVertexArray(hVAO);		// bind VAO
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);	// topology, size, type, start offset
-		glBindVertexArray(0);		// bind VAO
+		// render routine start
+		RenderTriangles(2, hVAO1, hShaderProgram1);
+		RenderTriangles(2, hVAO2, hShaderProgram2);
 
 		glfwSwapBuffers(hWindow);
 		glfwPollEvents();	// check event
 	}
 
 	// clean up resources
-	glDeleteVertexArrays(1, &hVAO);
-	glDeleteBuffers(1, &hVBO);	// vertex buffer delete
-	glDeleteProgram(hShaderProgram);
+	glDeleteVertexArrays(1, &hVAO1);
+	glDeleteVertexArrays(1, &hVAO2);
+	glDeleteBuffers(1, &hVBO);	// buffer delete
+	glDeleteBuffers(1, &hEBO1);	// buffer delete
+	glDeleteBuffers(1, &hEBO2);	// buffer delete
+	glDeleteProgram(hShaderProgram1);
+	glDeleteProgram(hShaderProgram2);
 
 	glfwTerminate();	// window destroy
 
@@ -227,7 +222,7 @@ void RegisterInputEvent(GLFWwindow* const hWindow)
 	// resizing
 	glfwSetFramebufferSizeCallback(hWindow, [](GLFWwindow* phWin, int w, int h) {
 		uintptr_t test = reinterpret_cast<uintptr_t>(phWin);
-		std::cerr << "debug: resize " << test << std::endl;
+		std::cout << "Event : resize window " << w << ' ' << h << ' ' << test << '\n';
 		glViewport(VIEWPORT_LD_X, VIEWPORT_LD_Y, w, h);
 		});	// register call-back, capture-less lambda implicitly converts to function pointer
 
@@ -241,6 +236,54 @@ void HandleInput(GLFWwindow* const hWindow)
 	// esc key input
 	if (glfwGetKey(hWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(hWindow, true);
+}
+
+GLuint CreateBO()
+{
+	GLuint hBO = 0;
+	glGenBuffers(1, &hBO);
+	return hBO;
+}
+
+GLuint CreateVBO(std::vector<Point> const& vertices)
+{
+	GLuint hVBO = CreateBO();
+
+	glBindBuffer(GL_ARRAY_BUFFER, hVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Point) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+	return hVBO;
+}
+
+GLuint CreateEBO(std::vector<uint32_t> const& indices)
+{
+	GLuint hEBO = CreateBO();
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, hEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * indices.size(), indices.data(), GL_STATIC_DRAW);
+	return hEBO;
+}
+
+GLuint CreateVAO(GLuint const hVBO, GLuint const hEBO)
+{
+	GLuint hVAO = 0;
+
+	glGenVertexArrays(1, &hVAO);
+
+	// start setting
+	glBindVertexArray(hVAO);
+
+	// EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, hEBO);
+
+	// VBO, input layout
+	glBindBuffer(GL_ARRAY_BUFFER, hVBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Point), reinterpret_cast<void*>(0));	// map vbo and shader program input at 0
+	glEnableVertexAttribArray(0); // enable layout of location 0, if another location exist another call needed
+
+	glBindVertexArray(0);
+	// end setting
+
+	return hVAO;
 }
 
 /**
@@ -285,11 +328,30 @@ GLuint CreateFragmentShader(char const* const shaderSource)
 	return hFragmentShader;
 }
 
-/**
- * @brief render object
- */
-void RenderObject()
+GLuint CreateShaderProgram(GLuint const hVShader, GLuint const hFShader)
 {
+	GLuint hShaderProgram = glCreateProgram();
+	glAttachShader(hShaderProgram, hVShader);
+	glAttachShader(hShaderProgram, hFShader);
+	glLinkProgram(hShaderProgram);	// link shaders, create executable
+	int isShaderLinkSuccess = 0;
+	glGetProgramiv(hShaderProgram, GL_LINK_STATUS, &isShaderLinkSuccess);
+	if (!isShaderLinkSuccess) {
+		//		glGetProgramInfoLog(hShaderProgram, 512, NULL, infoLog);
+		std::cerr << "Error : shader link failed." << std::endl;
+		return 0;
+	}
+	return hShaderProgram;
+}
 
+/**
+ * @brief render multiple triangles
+ */
+void RenderTriangles(GLsizei const numTri, GLuint const hVAO, GLuint const hShaderProgram)
+{
+	glUseProgram(hShaderProgram);	// bind shader program
+	glBindVertexArray(hVAO);		// bind VAO
+	glDrawElements(GL_TRIANGLES, numTri * 3, GL_UNSIGNED_INT, 0);	// topology, size, type, start offset
+	glBindVertexArray(0);		// bind VAO
 }
 
