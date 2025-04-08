3d205b9776b1040280117508e1af480fded77ec9
iphuck22 <eldeshue@naver.com>
2025-04-08 19:06:52 +0900

    feat: add shaders

---
+++ shader/FragmentShader.glsl
@@ -0,0 +1,14 @@
+
+#version 330 core
+
+in vec3 verColor;
+in vec2 texCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D ourTexture;
+
+void main()
+{
+	FragColor = texture(ourTexture, texCoord) * vec4(verColor, 1.0f);
+}
---
+++ shader/VertexShader.glsl
@@ -0,0 +1,15 @@
+
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aCol;
+layout (location = 2) in vec2 tPos;
+out vec3 verColor;
+out vec2 texCoord;
+
+void main()
+{
+   gl_Position = vec4(aPos, 1.0);
+   verColor = aCol;
+   texCoord = tPos;
+}
