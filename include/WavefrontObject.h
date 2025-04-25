306e7f31731df35403e93971349bac2174113e92
iphuck22 <eldeshue@naver.com>
2025-04-25 16:43:02 +0900

    feat : rendering buffer object implemented

---
+++ include/WavefrontObject.h
@@ -0,0 +1,52 @@
+
+#ifndef WAVEFRONT_OBJECT_H
+#define WAVEFRONT_OBJECT_H
+
+#include <vector>
+#include <array>
+#include <string>
+#include <glad/glad.h>
+
+using Float3 = std::array<float, 3>;
+using Float2 = std::array<float, 2>;
+
+using VecF3 = std::vector<Float3>;
+using VecF2 = std::vector<Float2>;
+
+// created by parser
+class WfObj
+{
+private:
+	WfObj(WfObj const&) = delete;
+	WfObj& operator=(WfObj const&) = delete;	// non copyable
+
+	// name of the file
+	std::string const name;
+
+	// buffer
+	VecF3 vPosBuf;
+	VecF3 vNormVecBuf;
+	VecF2 tPosBuf;
+	std::vector<uint32_t> indexBuf;
+
+	// handles
+	std::array<GLuint, 4> bOB;
+	GLuint hVAO;
+
+public:
+	WfObj(std::string const& name);
+	~WfObj();
+
+	// get
+	VecF3& getVPosBuffer();
+	VecF3& getNVecBuffer();
+	VecF2& getTexPosBuffer();
+	std::vector<uint32_t>& getIdxBuffer();
+
+	// init handles
+	void initHandles();
+
+	GLuint gethVAO() const;
+};
+
+#endif
